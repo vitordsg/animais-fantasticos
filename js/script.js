8217d4c839e063723f6b29f9720f5b2e83b65579
Vitor Santos <vitinho6.6@hotmail.com>
2023-08-19 18:46:30 -0300

    npm build

--- js/script.js
+++ js/script.js
@@ -37,5 +37,5 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-FetchAnimais('../../animaisapi.json', '.numeros-grid');
+FetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
